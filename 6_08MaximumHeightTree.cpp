86c0ef2daac876618d2edf32f622a27b41665a0e
nkc512 <nkc512@gmail.com>
2022-02-20 20:05:20 +0530

    MaximumHeightTree: get n from sum of 1-n numbers

---
+++ 6_08MaximumHeightTree.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+        int ans=sqrt(2*N);
+        int check=ans*(ans+1)/2;
+        return check<=N?ans:ans-1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
